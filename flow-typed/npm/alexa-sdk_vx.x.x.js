01cb88b92ba4a224f027cf63bed8f16746f29072
Scott Ranger <millenniumnight@googlemail.com>
2017-03-16 16:16:49 +0000

    Babel configuration for production and a few flow related things

--- flow-typed/npm/alexa-sdk_vx.x.x.js
+++ flow-typed/npm/alexa-sdk_vx.x.x.js
@@ -8,13 +8,15 @@
  *
  * Fill this stub out by replacing all the `any` types.
  *
- * Once filled out, we encourage you to share your work with the 
- * community by sending a pull request to: 
+ * Once filled out, we encourage you to share your work with the
+ * community by sending a pull request to:
  * https://github.com/flowtype/flow-typed
  */
 
 declare module 'alexa-sdk' {
-  declare module.exports: any;
+  declare module.exports: {
+      handler: (event: any, context: any) => any
+  };
 }
 
 /**
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 //@flow
 import Alexa from 'alexa-sdk';
 
-
 const handler = (event: any, context: any, callback: Function):void =>{
     var alexa = Alexa.handler(event, context);
 };
